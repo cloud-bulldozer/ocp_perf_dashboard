5274da247752a1ddf1f23ee04a98863c9b090bf2
MVarshini <varm@redhat.com>
2024-12-24 19:15:52 +0530

    OCP Filters

--- backend/app/api/v1/commons/constants.py
+++ backend/app/api/v1/commons/constants.py
@@ -39,3 +39,14 @@ FIELD_CONSTANT_DICT = {
     "upstream": "upstreamJob.keyword",
     "clusterType": "clusterType.keyword",
 }
+
+RELEASE_STREAM_DICT = {
+    "fast": "Fast",
+    "stable": "Stable",
+    "eus": "EUS",
+    "candidate": "Release Candidate",
+    "rc": "Release Candidate",
+    "nightly": "Nightly",
+    "ci": "ci",
+    "ec": "Engineering Candidate",
+}
